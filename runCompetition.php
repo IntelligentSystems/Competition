7d219120ae08b49db3910f346162e212e045af97
Laurens Rietveld <laurens.rietveld@vu.nl>
2013-01-31 08:56:44 +0100

    fixed some issues with stderr and stdout. all is fine now

--- runCompetition.php
+++ runCompetition.php
@@ -28,19 +28,36 @@ class Competition {
 		}
 		$this->log("Compiling " . count($this->groups) . " groups");
 		$this->preProcessGroups();
-		
-		
+		$this->clearPreviousTournamentResults();
 	}
 	
 	function run() {
-// 		$this->groups = range(1, 20);
+		$this->runRound($this->groups);
+	}
+	
+	function runRound($groups) {
 		$this->round++;
+		echo "\n### Playing round ".$this->round." ###\n";
 		$winners = array();
-		shuffle($this->groups);
-		for ($i = 0; $i < count($this->groups); $i++, $i++) {
-			$winners[] = $this->getWinner($this->groups[$i], $this->groups[$i+1]);
+		shuffle($groups);
+		for ($i = 0; $i < count($groups); $i++, $i++) {
+			$winners[] = $this->getWinner($groups[$i], $groups[$i+1]);
+		}
+		if (count($winners) === 1) {
+			$this->log("We have a winnerrrrr! \n##### group".reset($winners)." #####");
+		} else {
+			$this->runRound($winners);
 		}
 	}
+	
+	function clearPreviousTournamentResults() {
+		if (count(scandir($this->config['paths']['tournamentResults'])) > 3) {
+			//larger than 3, as we always have the paths '.', '..' and '.gitignore'
+			//we check this, because we get an shell err msg when the dir is empty upon removing the content
+			shell_exec("rm -r ".$this->config['paths']['tournamentResults']."*");
+		}
+	}
+
 	function getMaps() {
 		$mapPaths = glob($this->config['paths']['pwMaps']."*");
 		$maps = array();
@@ -56,6 +73,7 @@ class Competition {
 	 * @param unknown $group2
 	 */
 	function getWinner($group1, $group2) {
+		echo "games: ".$group1." - ".$group2."\n\t";
 		$results = array(
 			0 => 0, //for keeping track of draws...
 			$group1 => 0,
@@ -63,44 +81,43 @@ class Competition {
 		);
 		$this->prepareCompetitionDir($group1, $group2);
 		foreach ($this->maps as $map) {
-			
-			$results[$this->runGame($group1, $group2, $map)]++;
-			$results[$this->runGame($group2, $group1, $map)]++;
+			$gameWinner = $this->runGame($group1, $group2, $map);
+			echo $gameWinner." ";
+			$results[$gameWinner]++;
+			$gameWinner = $this->runGame($group2, $group1, $map);
+			echo $gameWinner." ";
+			$results[$gameWinner]++;
 		}
 		
-		$highestScore = -1;
-		$winner = -1;
-		foreach ($results as $id => $wins) {
-			if ($wins > $highestScore) {
-				$highestScore = $wins;
-				$winner = $id;
-			}
-		}
-		if ($winner <= 0) {
+		$roundWinner = 0;
+		
+		if ($results[$group1] === $results[$group2]) {
 			$rand = rand(1,2);
 			if ($rand === 1) { 
-				$winner = $group1;
+				$roundWinner = $group1;
 			} else {
-				$winner = $group2;
+				$roundWinner = $group2;
 			}
-			$this->log("game between ".$group1." - ".$group2.": DRAW. Flipping a coin, and the winner is.... ".$winner."!!");
+			echo("DRAW. Flipping a coin, and the winner is.... ".$roundWinner."!!\n");
+		} else if ($results[$group1] > $results[$group2]) {
+			$roundWinner = $group1;
+			echo("WINNER *** ".$group1." ***\n");
 		} else {
-			$this->log("game between ".$group1." - ".$group2.": WINNER *** ".$winner." ***");
+			$roundWinner = $group2;
+			echo("WINNER *** ".$group2." ***\n");
 		}
-		return $winner;
+		return $roundWinner;
 		
 	}
 	
 	function prepareCompetitionDir($group1, $group2) {
 		
-		$compDir = $this->config['paths']['competitionDir'];
-		$this->emptyDir($compDir); //clean before using
-// 		var_export($this->config['paths']['botsCompiled'].$group1);
-// 		var_export($compDir);exit;
-		$this->copyFilesInDir($this->config['paths']['botsCompiled'].$group1."/", $compDir);
-		$this->copyFilesInDir($this->config['paths']['botsCompiled'].$group2."/", $compDir);
-		$this->copyFilesInDir($this->config['paths']['pwEngine'], $compDir);
-		$this->copyFilesInDir($this->config['paths']['pwMaps'], $compDir);
+		$competitionDir = $this->config['paths']['competitionDir'];
+		$this->emptyDir($competitionDir); //clean before using
+		$this->copyFilesInDir($this->config['paths']['botsCompiled'].$group1."/", $competitionDir);
+		$this->copyFilesInDir($this->config['paths']['botsCompiled'].$group2."/", $competitionDir);
+		$this->copyFilesInDir($this->config['paths']['pwEngine'], $competitionDir);
+		$this->copyFilesInDir($this->config['paths']['pwMaps'], $competitionDir);
 	}
 	
 	function runGame($player1, $player2, $map) {
@@ -110,21 +127,36 @@ class Competition {
 		if (!strlen($player2BotName)) $this->error("Unable to retrieve bot name for group ".$player2);
 		
 		$workingDir = getcwd();
+		$resultFile = $player1."-".$player2."_".$map;
 		chdir($this->config['paths']['competitionDir']);
 		$cmd = "java -jar PlayGame.jar ".$map;
 		$cmd .= " \"java -Xmx" . $this->config['game']['maxMemory'] ."m ".$player1BotName."\" \"java -Xmx" . $this->config['game']['maxMemory'] ."m ".$player2BotName."\"";
-		$cmd .= " parallel ".$this->config['game']['numTurns']." ".$this->config['game']['maxTurnTime']." 2>&1";
-		$resultString = shell_exec($cmd);
+		$cmd .= " parallel ".$this->config['game']['numTurns']." ".$this->config['game']['maxTurnTime'];
+		
+		//cannot just pipe stderr to stdout: we need seperate pipes. stderr for analyzing who won, and stdout for the visualizer
+		$descriptorspec = array(
+				0 => array("pipe", "r"),  // stdin
+				1 => array("pipe", "w"),  // stdout
+				2 => array("pipe", "w"),  // stderr
+		);
+		$process = proc_open($cmd, $descriptorspec, $pipes);
+		$gameStatesString = stream_get_contents($pipes[1]); //stdout
+		fclose($pipes[1]);
+		$resultString = stream_get_contents($pipes[2]);//stderr
+		fclose($pipes[2]);
+		proc_close($process);
+		
 		chdir($workingDir);
 		
-		$this->storeGameResult($player1, $player2, $map, $resultString);
-		return $this->getGameResult($resultString, $player1, $player2);
+		$winner = $this->getGameResult($resultString, $player1, $player2);
+		$this->storeGameResult($player1, $player2, $winner, $map, $gameStatesString);
+		return $winner;
 		
 		
 		//reset to original working directory
 	}
 	
-	function storeGameResult($player1, $player2, $map, $resultString) {
+	function storeGameResult($player1, $player2, $winner, $map, $resultString) {
 		$resultDir = $this->config['paths']['tournamentResults'];
 		$roundDir = $resultDir.$this->round."/";
 		if (!is_dir($roundDir)) mkdir($roundDir);
@@ -132,12 +164,12 @@ class Competition {
 		$batchResultsDir = $roundDir.min($player1, $player2)."-".max($player1, $player2)."/";
 		if (!is_dir($batchResultsDir)) mkdir($batchResultsDir);
 		
-		$gameFile = $batchResultsDir.$player1."-".$player2."_".$map;
+		$gameFile = $batchResultsDir."w".$winner."_".$player1."-".$player2."_".$map; 
 		file_put_contents($gameFile, $resultString);
 	}
 	
 	function getGameResult($resultString, $player1, $player2) {
-		if (strpos($result, "Draw") !== false) {
+		if (strpos($resultString, "Draw") !== false) {
 			//we have a draw...
 			return 0;
 		} else {
@@ -145,8 +177,9 @@ class Competition {
 			preg_match($pattern, $resultString, $matches);
 			$winner = (int)end($matches);
 			if ($winner < 1 || $winner > 2) {
-				$this->error("Unable to parse output results. Who is the winner?? Output: ".$resultStirng);
+				$this->error("Unable to parse output results. Who is the winner?? Output: ".$resultString);
 			}
+			$winner = ($winner === 1? $player1: $player2);
 			return $winner;
 		}
 	}
@@ -197,7 +230,10 @@ class Competition {
 		$handle = opendir($fromDir);
 		while (($file = readdir($handle)) != false) {
 			if (!is_dir($destDir)) mkdir($destDir);
-			if ($file != "." && $file != "..") {
+			if ($file != "." && $file != ".." && $file != ".gitignore") {
+				if (is_dir($fromDir.$file)) {
+					$this->error("Trying to copy directory here. What's wrong? ".$fromDir.$file);
+				}
 				copy($fromDir.$file, $destDir.$file);
 			}
 		}
@@ -264,7 +300,7 @@ class Competition {
 	}
 	
 	function log($message) {
-		echo "== ".$message." ==\n";
+		echo "** ".$message." \n";
 	}
 	
 	function error($message) {
